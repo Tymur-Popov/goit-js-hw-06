496d496eefc66a3fe2de156274230f559f728577
Tymur Popov <bs2timur@gmail.com>
2022-05-14 20:51:28 +0300

    update

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+function onButtonClick () {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorName.textContent = color;
+};
+
+changeColorBtn.addEventListener("click", onButtonClick);
+
